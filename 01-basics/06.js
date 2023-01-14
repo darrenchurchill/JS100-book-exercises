6323312946a35c14d66813b72839762ecd798cda
Darren Churchill <darrenmchurchill@gmail.com>
2023-01-14 11:42:27 -0800

    Ch 1 Exercise 6

---
+++ 01-basics/06.js
@@ -0,0 +1,12 @@
+/**
+ * Launch School Introduction to Programming with JavaScript
+ * Ch. 1: The Basics
+ * Exercise 6
+ *
+ * Use the template literal syntax along with the expression `Number('5') + 10`
+ * to log the following sentence to the console:
+ *
+ * `The value of 5 + 10 is 15.`
+ */
+
+console.log(`The value of 5 + 10 is ${Number('5') + 10}.`)
