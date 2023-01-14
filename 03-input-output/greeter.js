d6e40a8f9b3ee625e4df5bbce78f97501c30032e
Darren Churchill <darrenmchurchill@gmail.com>
2023-01-14 14:17:52 -0800

    Ch 3 Exercise 2 - update greeter.js

--- 03-input-output/greeter.js
+++ 03-input-output/greeter.js
@@ -1,7 +1,7 @@
 /**
  * Launch School Introduction to Programming with JavaScript
  * Ch. 3: Input/Output
- * Exercise 1
+ * Exercise 1/2
  *
  * Write a dynamic greeter program named greeter.js. The program should ask for
  * your name, then output Hello, {name}! where {name} is the name you entered.
@@ -9,5 +9,7 @@
 
 let rlsync = require('readline-sync');
 
-let username = rlsync.question('What is your name? ');
-console.log(`Hello ${username}!`);
+let firstname = rlsync.question('What is your first name? ');
+let lastname = rlsync.question('What is your last name? ');
+
+console.log(`Hello ${firstname} ${lastname}!`);
