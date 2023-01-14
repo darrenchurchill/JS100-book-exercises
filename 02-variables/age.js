621f3798ee0fc4a2346f68881220a3419031c753
Darren Churchill <darrenmchurchill@gmail.com>
2023-01-14 13:21:56 -0800

    Ch 2 Exercise 2

---
+++ 02-variables/age.js
@@ -0,0 +1,21 @@
+/**
+ * Launch School Introduction to Programming with JavaScript
+ * Ch. 2: Variables
+ * Exercise 2
+ *
+ * Write a program named age.js that includes someone's age and then calculates
+ * and reports their future age in 10, 20, 30, and 40 years.
+ */
+
+const age = 32;
+let numYears = 10;
+console.log(`In ${numYears} years, you will be ${age + numYears} years old.`)
+
+numYears += 10;
+console.log(`In ${numYears} years, you will be ${age + numYears} years old.`)
+
+numYears += 10;
+console.log(`In ${numYears} years, you will be ${age + numYears} years old.`)
+
+numYears += 10;
+console.log(`In ${numYears} years, you will be ${age + numYears} years old.`)
