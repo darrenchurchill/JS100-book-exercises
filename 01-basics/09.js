b9434c021fc5970f1549c6b3a0f13e544d55f80b
Darren Churchill <darrenmchurchill@gmail.com>
2023-01-14 12:00:18 -0800

    Ch 1 Exercise 9

---
+++ 01-basics/09.js
@@ -0,0 +1,16 @@
+/**
+ * Launch School Introduction to Programming with JavaScript
+ * Ch. 1: The Basics
+ * Exercise 9
+ *
+ * Create an object named `names` that contains a list of pet names and the type
+ * of animal.
+ */
+
+let pets = {
+  asta: 'dog',
+  butterscotch: 'cat',
+  pudding: 'cat',
+  neptune: 'fish',
+  darwin: 'lizard',
+};
