4302f6d0606bab01af35bd8674e74b4d6e6d0977
Darren Churchill <darrenmchurchill@gmail.com>
2023-01-14 11:50:04 -0800

    Ch 1 Exercise 8

---
+++ 01-basics/08.js
@@ -0,0 +1,9 @@
+/**
+ * Launch School Introduction to Programming with JavaScript
+ * Ch. 1: The Basics
+ * Exercise 8
+ *
+ * Create an array named `names` that contains a list of pet names.
+ */
+
+let pets = ['asta', 'butterscotch', 'pudding', 'neptune', 'darwin'];
