8b77a5742e510d9228f27aa240f2ff4b5c6f33d2
Darren Churchill <darrenmchurchill@gmail.com>
2023-01-14 13:13:17 -0800

    Ch 2 Exercise 1

---
+++ 02-variables/greeter.js
@@ -0,0 +1,15 @@
+/**
+ * Launch School Introduction to Programming with JavaScript
+ * Ch. 2: Variables
+ * Exercise 1
+ *
+ * Write a program named greeter.js that greets `'Victor'` three times.
+ *
+ * Use a variable and don't hard code the string value.
+ */
+
+const username = 'Victor';
+
+console.log(`Good Morning, ${username}.`);
+console.log(`Good Afternoon, ${username}.`);
+console.log(`Good Evening, ${username}.`);
