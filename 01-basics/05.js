05bbaa2a5f30624448ac1724cd9abd1800638d69
Darren Churchill <darrenmchurchill@gmail.com>
2023-01-14 11:37:31 -0800

    Ch 1 Exercise 5

---
+++ 01-basics/05.js
@@ -0,0 +1,10 @@
+/**
+ * Launch School Introduction to Programming with JavaScript
+ * Ch. 1: The Basics
+ * Exercise 5
+ *
+ * Refactor the code from Exercise 4 so it logs `15` instead.
+ * `console.log('5' + 10);`
+ */
+
+console.log(Number('5') + 10);
