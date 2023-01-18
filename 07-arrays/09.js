d037072060780c58f34a3796e8ca367f7becf2f7
Darren Churchill <darrenmchurchill@gmail.com>
2023-01-18 09:17:25 -0800

    Refactor Ch 7 Exercise 9 to use `Array.includes` instead

--- 07-arrays/09.js
+++ 07-arrays/09.js
@@ -7,14 +7,10 @@
  * checks whether the number `3` appears inside an array.
  */
 
-function isInArray(array, num) {
-  return array.indexOf(num) > 0;
-}
-
 let numbers1 = [1, 3, 5, 7, 9, 11];
 let numbers2 = [];
 let numbers3 = [2, 4, 6, 8];
 
-console.log(isInArray(numbers1, 3));
-console.log(isInArray(numbers2, 3));
-console.log(isInArray(numbers3, 3));
+console.log(numbers1.includes(3));
+console.log(numbers2.includes(3));
+console.log(numbers3.includes(3));
