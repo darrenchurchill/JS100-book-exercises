1c36d2fd77d44119b32254b05e65b41edc9b1002
Darren Churchill <darrenmchurchill@gmail.com>
2023-01-18 14:55:07 -0800

    Ch 9 Exercise 3

---
+++ 09-more-stuff/03.js
@@ -0,0 +1,9 @@
+/**
+ * Launch School Introduction to Programming with JavaScript
+ * Ch. 9: More Stuff
+ * Exercise 3
+ *
+ * Write some code to output the square root of 37.
+ */
+
+console.log(Math.sqrt(37));
