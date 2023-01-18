6f396c9e0ef0460b2cb51a295869a7a36d3c9e52
Darren Churchill <darrenmchurchill@gmail.com>
2023-01-18 12:45:33 -0800

    Complete Ch 8 Exercise 13

--- 08-objects/13.js
+++ 08-objects/13.js
@@ -10,6 +10,6 @@ function foo(bar) {
   console.log(bar());
 }
 
-foo();    // Should print 'Welcome'
-foo();    // Should print 3.1415
-foo();    // Should print [1, 2, 3]
+foo(() => 'Welcome');    // Should print 'Welcome'
+foo(() => 3.1415);       // Should print 3.1415
+foo(() => [1, 2, 3]);    // Should print [1, 2, 3]
