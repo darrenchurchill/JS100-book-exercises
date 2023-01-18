fa0ac7d5d2818f8e6f1686f3154cd7277804a188
Darren Churchill <darrenmchurchill@gmail.com>
2023-01-18 12:43:44 -0800

    Add Ch 8 Exercise 13

---
+++ 08-objects/13.js
@@ -0,0 +1,15 @@
+/**
+ * Launch School Introduction to Programming with JavaScript
+ * Ch. 8: Objects
+ * Exercise 13
+ *
+ * Without modifying `foo`, update the code to print the desired text.
+ */
+
+function foo(bar) {
+  console.log(bar());
+}
+
+foo();    // Should print 'Welcome'
+foo();    // Should print 3.1415
+foo();    // Should print [1, 2, 3]
