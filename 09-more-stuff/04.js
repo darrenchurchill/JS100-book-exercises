53a7855c63e898f3e5f3fdc9f3f9aac13e0e34f0
Darren Churchill <darrenmchurchill@gmail.com>
2023-01-18 14:59:37 -0800

    Ch 9 Exercise 4

---
+++ 09-more-stuff/04.js
@@ -0,0 +1,16 @@
+/**
+ * Launch School Introduction to Programming with JavaScript
+ * Ch. 9: More Stuff
+ * Exercise 4
+ *
+ * Given a list of numbers, write some code to find and display the largest
+ * numeric value in the list.
+ */
+
+list1 = [1, 6, 3, 2];
+list2 = [-1, -6, -3, -2];
+list3 = [2, 2];
+
+console.log(Math.max(...list1));
+console.log(Math.max(...list2));
+console.log(Math.max(...list3));
