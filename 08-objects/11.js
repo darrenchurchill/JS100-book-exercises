04de8aeb8afac9621eee1acae79f5bcd92bcda8f
Darren Churchill <darrenmchurchill@gmail.com>
2023-01-18 12:24:52 -0800

    Ch 8 Exercise 11

---
+++ 08-objects/11.js
@@ -0,0 +1,19 @@
+/**
+ * Launch School Introduction to Programming with JavaScript
+ * Ch. 8: Objects
+ * Exercise 11
+ *
+ * Write some code to replace the value 6 in the following object with 606.
+ * You don't have to search the object, just write an assignment that replaces
+ * the 6.
+ */
+
+let obj = {
+  foo: { a: "hello", b: "world"},
+  bar: ["example", "mem", null, { xyz: 6 }, 88],
+  qux: [4, 8, 12]
+};
+
+obj.bar[3].xyz = 606;
+
+console.log(obj);
