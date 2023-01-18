7e70a4215a3866e12100308cecd02a10978d3df0
Darren Churchill <darrenmchurchill@gmail.com>
2023-01-18 12:35:13 -0800

    Add Ch 8 Exercise 12

---
+++ 08-objects/12.js
@@ -0,0 +1,15 @@
+/**
+ * Launch School Introduction to Programming with JavaScript
+ * Ch. 8: Objects
+ * Exercise 12
+ *
+ * Without creating a new function or changing the calls to `foo`, update the
+ * code to fix the error and work as intended.
+ */
+
+function foo(bar) {
+  console.log(bar, bar, bar);
+}
+
+foo("hello");  // should print "hello hello hello"
+bar("hi");  // should print "hi hi hi"
